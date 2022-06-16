4ed5406554cb2812b37c1fda4b7f4bdc074edf5a
violanerd <madalyne.cross@gmail.com>
2022-06-16 16:15:54 -0500

    finished writing questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,25 @@ var question2 = {
     question: "Which for loop has the correct syntax?",
     answers: ["a. for (i = 0; i < n; i++)", "b. for (var i = 0; i < n; i++)", "c. for var i = 0, i < n, i++", "d. for (var i = 0, i < n, i++)", "e. none of the above"],
     correct: "b. for (var i = 0; i < n; i++)"
-}
+};
+
+var question3 = {
+    question: "Choose the comparison operator that means strict equality:",
+    answers: ["a. >=", "b. ==", "c. =", "d. ===", "e. none of the above"],
+    correct: "d. ==="
+};
+
+var question4 = {
+    question: "Given this array --> var cheese =['cheddar', 'gouda', 'parmesan', 'feta']', --> What statement will return 'cheddar'?",
+    answers: ["a. console.log('cheese');", "b. console.log(cheese[3]);", "c. console.log(cheese[0]);", "d. console.log('cheddar')", "e. c and d"],
+    correct: "e. c and d"
+};
+var question5 = {
+    question: "What does window.confirm('Are you sure?') return?",
+    answers: ["a. yes or no", "b. ok or cancel", "c. true or false", "d. all of the above", "e. none of the above"],
+    correct: "c. true or false"
+};
+
 
 var startQuiz = document.querySelector("#startQuiz");
 startQuiz.addEventListener("click", function() {
