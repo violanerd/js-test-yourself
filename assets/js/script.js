b982a04fea88cd2cea07fac17299c9fadc47e494
violanerd <madalyne.cross@gmail.com>
2022-06-18 14:40:43 -0500

    added loop through question functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,32 +26,57 @@ var question5 = {
     answers: ["a. yes or no", "b. ok or cancel", "c. true or false", "d. all of the above", "e. none of the above"],
     correct: "c. true or false"
 };
+// an array to hold the questions
 var questionArray =[question1, question2, question3, question4, question5];
 
+// to start the quiz
 var startQuizEl = document.querySelector("#start-wrapper");
+
+// start screen content to erase it
 var welcomePEl = document.querySelector("#start-screen-content");
+
+// ?
 var answerEl = document.querySelector("#answer");
+
+// selects where to listen for answer choice click
 var questionEl = document.querySelector("#question-content");
+
+// where the question number is announced 
 var announcement = document.querySelector("#announcer");
+
+// where the question is written
 var question = document.querySelector("#question");
+
+//where the answers are written
 var answerContainerEl = document.querySelector("#answer-wrapper");
+
+// validation
 var correctEl = document.querySelector("#correct");
 var incorrectEl = document.querySelector("#incorrect");
 
+// variable to use for iterating through questions
+var questionNum = 0;
 
 var startQuiz = function () {
     startQuizEl.textContent=("");
     welcomePEl.textContent=(""); 
-    announcement.textContent=("Question 1");
-    question.textContent=questionArray[0].question;
+
+    generateQuestions();
+    
+    //add code to display a timer
+};
+
+function generateQuestions () {
+    //code to generate question/answers
+    announcement.textContent=("Question " + (questionNum + 1).toString());
+    question.textContent=questionArray[questionNum].question;
     // display answers
-    var ansArray = questionArray[0].answers
+    var ansArray = questionArray[questionNum].answers
     for (var i = 0; i < ansArray.length; i++){
         var ansButtonEl = document.createElement("button");
         ansButtonEl.textContent = ansArray[i];
         answerContainerEl.appendChild(ansButtonEl);
     }
-    //add code to display a timer
 };
 
 var validate = function(event) {     
@@ -62,10 +87,11 @@ var validate = function(event) {
     } else if (element.textContent!=question1.correct) {
         incorrectEl.textContent = "Nope!";
     }
+    answerContainerEl.textContent="";
+    questionNum++;
+    generateQuestions();
 };
 
-// display correct
-// display incorrect
 
 // function to end the game when the timer hits zero **  OR all questions are answered 
 // this will be a conditional OR 
